65d77b3a20596d1bca84a48cf09825408d705a93
kelche <kelcheone@gmail.com>
2024-08-05 23:07:28 +0300

    feat: init the node and the badger database instance

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	cfg "github.com/cometbft/cometbft/config"
+	cmtflags "github.com/cometbft/cometbft/libs/cli/flags"
+	cmtlog "github.com/cometbft/cometbft/libs/log"
+	nm "github.com/cometbft/cometbft/node"
+	"github.com/cometbft/cometbft/p2p"
+	"github.com/cometbft/cometbft/privval"
+	"github.com/cometbft/cometbft/proxy"
+	"github.com/dgraph-io/badger/v4"
+	"github.com/spf13/viper"
+)
+
+var homedir string
+
+func init() {
+	flag.StringVar(&homedir, "cmt-home", "", "Path to the CometBFT config directory (if empty, uses $HOME/.cometbft/")
+}
+
+func main() {
+	flag.Parse()
+	if homedir == "" {
+		homedir = os.ExpandEnv("$HOME/.cometbft/")
+	}
+	config := cfg.DefaultConfig()
+	config.SetRoot(homedir)
+
+	configTomlPath := filepath.Join(homedir, "config/config.toml")
+	log.Println(configTomlPath)
+	// viper.SetConfigFile(fmt.Sprintf("%s%s", homedir, "config/config.toml"))
+	viper.SetConfigFile(configTomlPath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Reading config: %v", err)
+	}
+
+	if err := viper.Unmarshal(config); err != nil {
+		log.Fatalf("Decoding config: %v", err)
+	}
+
+	if err := config.ValidateBasic(); err != nil {
+		log.Fatalf("Invalid configuration data: %v", err)
+	}
+	dbPath := filepath.Join(homedir, "badger")
+	db, err := badger.Open(badger.DefaultOptions(dbPath))
+	if err != nil {
+		log.Fatalf("Opening database: %v", err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatalf("Closing databae: %v", err)
+		}
+	}()
+
+	app := NewKVStoreApplication(db)
+
+	pv := privval.LoadFilePV(
+		config.PrivValidatorKeyFile(),
+		config.PrivValidatorStateFile(),
+	)
+
+	nodeKey, err := p2p.LoadNodeKey(config.NodeKeyFile())
+	if err != nil {
+		log.Fatalf("failed to load node's key: %v", err)
+	}
+
+	logger := cmtlog.NewTMLogger(cmtlog.NewSyncWriter(os.Stdout))
+	logger, err = cmtflags.ParseLogLevel(config.LogLevel, logger, cfg.DefaultLogLevel)
+	if err != nil {
+		log.Fatalf("failed to parse log level: %v", err)
+	}
+
+	node, err := nm.NewNode(
+		config,
+		pv,
+		nodeKey,
+		proxy.NewLocalClientCreator(app),
+		nm.DefaultGenesisDocProviderFunc(config),
+		cfg.DefaultDBProvider,
+		nm.DefaultMetricsProvider(config.Instrumentation),
+		logger,
+	)
+	if err != nil {
+		log.Fatalf("Creating node: %v", err)
+	}
+
+	node.Start()
+
+	defer func() {
+		node.Stop()
+		node.Wait()
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+}
